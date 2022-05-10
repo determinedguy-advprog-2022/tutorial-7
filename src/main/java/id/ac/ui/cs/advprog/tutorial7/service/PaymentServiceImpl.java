d1781edc3f943b5cb8d2851bf07836216a46c2cc
Muhammad Athallah <mhd.athallah@gmail.com>
2022-05-10 20:30:38 +0700

    Make things asynchronous (the right way)

--- src/main/java/id/ac/ui/cs/advprog/tutorial7/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/tutorial7/service/PaymentServiceImpl.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.tutorial7.service;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Stream;
@@ -35,104 +36,52 @@ public class PaymentServiceImpl implements PaymentService{
 
     @Override
     public PaymentResponse pay(String va, int payAmount, String day, String time) {
-        AtomicInteger vaAmount = new AtomicInteger();
-        AtomicReference<BankApi> bankApi = new AtomicReference<>();
+        CompletableFuture<Boolean> isHolidayFuture = CompletableFuture.supplyAsync(() -> holidayApi.isHoliday(day));
+        CompletableFuture<Integer> vaAmountFuture = CompletableFuture.supplyAsync(() -> vaHelper.getVAAmount(va));
+        CompletableFuture<BankApi> bankFuture = CompletableFuture.supplyAsync(() -> vaHelper.getBankByVA(va));
+        CompletableFuture<String> paymentValidateFuture;
+        CompletableFuture<Boolean> bankClosedFuture;
 
-        PaymentResponse successStatus = new PaymentResponse(1, "");
+        int vaAmount;
+        BankApi bankApi;
+        boolean paymentSuccessful = false;
 
-        CompletableFuture<PaymentResponse> holidayStatus = CompletableFuture.supplyAsync(() -> {
-                if (holidayApi.isHoliday(day)) {
-                    return new PaymentResponse(0, "Cannot pay on holidays");
-                }
-                return successStatus;
+        try {
+            bankApi = bankFuture.get();
+            bankClosedFuture = CompletableFuture.supplyAsync(() -> bankApi.isBankClosed(time, payAmount));
+            vaAmount = vaAmountFuture.get();
+            paymentValidateFuture = CompletableFuture.supplyAsync(() -> vaHelper.validatePayment(va, vaAmount, payAmount));
+
+            if (isHolidayFuture.get()) {
+                return new PaymentResponse(0, "Cannot pay on holidays");
             }
-        );
 
-        CompletableFuture<PaymentResponse> vaStatus = CompletableFuture.supplyAsync(() -> {
-                try {
-                    vaAmount.set(vaHelper.getVAAmount(va));
-                    bankApi.set(vaHelper.getBankByVA(va));
-                } catch (NoSuchElementException e) {
-                    return new PaymentResponse(0, "VA number not found");
-                }
-                return successStatus;
+            if ((bankClosedFuture.get())) {
+                return new PaymentResponse(0, "Bank already closed, please try again tomorrow");
             }
-        );
 
-        CompletableFuture<PaymentResponse> bankStatus = CompletableFuture.supplyAsync(() -> {
-                if (bankApi.get().isBankClosed(time, vaAmount.get())) {
-                    return new PaymentResponse(0, "Bank already closed, please try again tomorrow");
-                }
-                return successStatus;
+            String errorMsg = paymentValidateFuture.get();
+
+            if (!errorMsg.equals("")) {
+                return new PaymentResponse(0, errorMsg);
             }
-        );
 
-        CompletableFuture<PaymentResponse> paymentValidation = CompletableFuture.supplyAsync(() -> {
-            String errorMsg = vaHelper.validatePayment(va, vaAmount.get(), payAmount);
-                if (!errorMsg.equals("")) {
-                    return new PaymentResponse(0, errorMsg);
-                }
-                return successStatus;
-            }
-        );
+            CompletableFuture<Boolean> bankPaymentFuture = CompletableFuture.supplyAsync(() -> bankApi.pay(payAmount));
+            paymentSuccessful = bankPaymentFuture.get();
 
-        CompletableFuture<PaymentResponse> paymentStatus = CompletableFuture.supplyAsync(() -> {
-                boolean paymentSuccessful = bankApi.get().pay(payAmount);
-                vaHelper.logVAPayment(va, paymentSuccessful);
-                if(!paymentSuccessful) {
-                    return new PaymentResponse(0, "Payment unsuccessful, please try again");
-                }
-                return new PaymentResponse(1, "Payment successful");
-            }
-        );
+        } catch (NoSuchElementException e) {
+            return new PaymentResponse(0, "VA number not found");
+        } catch (InterruptedException | ExecutionException ignored){
 
-        // https://stackoverflow.com/a/67445305
-        CompletableFuture<Void> combinedFuture =
-                CompletableFuture.allOf(holidayStatus, vaStatus, bankStatus, paymentValidation, paymentStatus);
-
-        combinedFuture.join();
-
-        List<PaymentResponse> answers = Stream.of(holidayStatus, vaStatus, bankStatus, paymentValidation, paymentStatus)
-                .map(CompletableFuture::join).toList();
-
-        if (answers.get(0).ok == 0) {
-            return answers.get(0);
-        } else if (answers.get(1).ok == 0) {
-            return answers.get(1);
-        } else if (answers.get(2).ok == 0) {
-            return answers.get(2);
-        } else if (answers.get(3).ok == 0) {
-            return answers.get(3);
         }
-        return answers.get(4);
 
-        // Check if it is holiday
-        // if(holidayApi.isHoliday(day)) return new PaymentResponse(0, "Cannot pay on holidays");
+        boolean paymentSuccessfulFinal = paymentSuccessful;
+        CompletableFuture.runAsync(() -> vaHelper.logVAPayment(va, paymentSuccessfulFinal));
 
-        // Validate VA number
-//        int vaAmount;
-//        BankApi bankApi;
-//
-//        try {
-//            vaAmount = vaHelper.getVAAmount(va);
-//            bankApi = vaHelper.getBankByVA(va);
-//        } catch(NoSuchElementException e) {
-//            return new PaymentResponse(0, "VA number not found");
-//        }
-
-        // Check if bank is open or closed
-        // if(bankApi.isBankClosed(time, vaAmount)) return new PaymentResponse(0, "Bank already closed, please try again tomorrow");
-
-        // Validate payment
-//        String errorMsg = vaHelper.validatePayment(va, vaAmount.get(), payAmount);
-//        if(!errorMsg.equals("")) return new PaymentResponse(0, errorMsg);
-
-        // Check if payment is valid
-//        boolean paymentSuccessful = bankApi.get().pay(payAmount);
-//        vaHelper.logVAPayment(va, paymentSuccessful);
-//        if(!paymentSuccessful) return new PaymentResponse(0, "Payment unsuccessful, please try again");
-//        else return new PaymentResponse(1, "Payment successful");
-
+        if (paymentSuccessful) {
+            return new PaymentResponse(1, "Payment successful");
+        }
+        return new PaymentResponse(0, "Payment unsuccessful, please try again");
     }
     
 }
